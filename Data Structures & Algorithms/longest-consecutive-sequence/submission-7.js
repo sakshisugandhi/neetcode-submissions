7cc5e7c7e5fd0bcc364a914e02dddc4928e44de1
Sakshi Sugandhi <104353215+sakshisugandhi@users.noreply.github.com>
2026-05-08 16:55:21 +0530

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.js
@@ -0,0 +1,53 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const sortedArray = [...new Set(nums.sort((a, b) => a - b))];
+        if (sortedArray.length === 1 || sortedArray.length === 0) {
+            return sortedArray.length;
+        }
+        let maxCount = 0;
+        let count = 0;
+        for (let i = 0; i < sortedArray.length; i++) {
+            if (i === 0 && sortedArray[i + 1] - sortedArray[i] === 1) {
+                count++;
+            } else if (
+                sortedArray[i + 1] - sortedArray[i] === 1 &&
+                sortedArray[i] - sortedArray[i - 1] === 1
+            ) {
+                count++;
+            } 
+            // else if (i === sortedArray.length - 1 && sortedArray[i] - sortedArray[i - 1] === 1) {
+            //     count++;
+            // } 
+            else if (
+                sortedArray[i] - sortedArray[i - 1] === 1 &&
+                sortedArray[i + 1] - sortedArray[i] !== 1
+            ) {
+                count++;
+            } else if (
+                sortedArray[i] - sortedArray[i - 1] !== 1 &&
+                sortedArray[i + 1] - sortedArray[i] === 1
+            ) {
+                if (count > maxCount) {
+                    maxCount = count;
+                }
+                count = 1;
+            } else {
+                if (count > maxCount) {
+                    maxCount = count;
+                }
+                count = 0;
+            }
+        }
+        if (maxCount > count) {
+            return maxCount;
+        } else if (count === 0) {
+            return count + 1;
+        } else {
+            return count;
+        }
+    }
+}
