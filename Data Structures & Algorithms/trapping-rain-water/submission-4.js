d413aa1792dbb840130f41054b07934ed5833988
Sakshi Sugandhi <104353215+sakshisugandhi@users.noreply.github.com>
2026-05-13 10:58:47 +0530

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let leftMax = 0;
+        let area = 0;
+        for (let i = 0; i < height.length; i++) {
+                        leftMax = Math.max(leftMax, height[i]);
+            if (i == 0 || i == height.length - 1) {
+                continue;
+            }
+            let rightMax = 0;
+            for (let j = height.length - 1; j > i; j--) {
+                rightMax = Math.max(rightMax, height[j]);
+            }
+            let water = Math.min(leftMax, rightMax) - height[i];
+            console.log(water , "water")
+            if (water > 0) {
+                area += water;
+            }
+        }
+        return area;
+    }
+}
